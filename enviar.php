963521984d63f5052916d0787078d31382579fd4
Carlos456974 <kenaygarciaa@gmail.com>
2025-05-27 13:48:21 -0600

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,1 +1,11 @@
+<?php
+$nombre = trim($_POST['nombre']) ?: "Anónimo";
+$mensaje = trim($_POST['mensaje']);
+$fecha = date("Y-m-d H:i:s");
+
+$registro = "[$fecha] [$nombre]\n$mensaje\n\n";
+file_put_contents("buzon.txt", $registro, FILE_APPEND);
+
+echo "Queja enviada con éxito. <a href='index.php'>Volver</a>";
+?>
 
