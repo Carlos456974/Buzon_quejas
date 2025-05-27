9c1977f99ba87a2351084948d872a0e097b52cf7
Carlos456974 <kenaygarciaa@gmail.com>
2025-05-27 13:48:41 -0600

    Update panel.php

--- panel.php
+++ panel.php
@@ -1,1 +1,18 @@
+<?php
+session_start();
+if (!isset($_SESSION['admin'])) {
+    header("Location: admin.php");
+    exit();
+}
+
+echo "<h2>Panel de Quejas</h2>";
+echo "<a href='logout.php'>Cerrar sesión</a><br><br>";
+
+$archivo = "buzon.txt";
+if (file_exists($archivo)) {
+    echo "<pre>" . htmlspecialchars(file_get_contents($archivo)) . "</pre>";
+} else {
+    echo "Aún no hay quejas registradas.";
+}
+?>
 
