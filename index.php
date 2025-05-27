66127b245169e12274bce94834b44c75946ee3c6
Carlos456974 <kenaygarciaa@gmail.com>
2025-05-27 13:47:40 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
+<form action="enviar.php" method="POST">
+    <h2>Buzón de Quejas</h2>
+    <label>Nombre (opcional):</label><br>
+    <input type="text" name="nombre"><br><br>
+
+    <label>Mensaje:</label><br>
+    <textarea name="mensaje" rows="5" required></textarea><br><br>
+
+    <input type="submit" value="Enviar Queja">
+</form>
 
